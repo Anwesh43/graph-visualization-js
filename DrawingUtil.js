5f14d45b305778bc09fe779169d14f63fa6cf11b
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 15:46:37 +0530

    adding Animator class

--- DrawingUtil.js
+++ DrawingUtil.js
@@ -54,3 +54,24 @@ class State {
         }
     }
 }
+
+class Animator {
+
+    constructor() {
+        this.animated = false
+    }
+
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, 50)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
