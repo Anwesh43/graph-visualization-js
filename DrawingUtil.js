d6aba4e1dfa5a8027d2c42adb3681da638cca48b
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 17:09:13 +0530

    drawing edges

--- DrawingUtil.js
+++ DrawingUtil.js
@@ -5,6 +5,9 @@ const strokeFactor = 90
 const sizeFactor = 20
 const backColor = '#bdbdbd'
 var data = 0
+const foreColor = "#283593"
+const r = Math.min(w, h) / sizeFactor
+const fontSizeFactor = 1
 
 class Stage {
     constructor() {
@@ -23,7 +26,7 @@ class Stage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
-        this.graphRenderer.draw(context)
+        this.graphRenderer.draw(this.context)
     }
 
     handleTap() {
@@ -90,10 +93,18 @@ class GraphPoint {
     }
 
     draw(context) {
-        const r = Math.min(w, h) /
+
+        context.fillStyle = foreColor
         context.beginPath()
-        context.arc(x, y, r * this.state.scale, 0, 2 * Math.PI)
+        context.arc(this.x, this.y, r * this.state.scale, 0, 2 * Math.PI)
         context.fill()
+        context.fillStyle = backColor
+        if (this.state.scale >= 1) {
+            const fontSize = Math.floor(r / fontSizeFactor)
+            context.font = context.font.replace(/\d{2}/, Math.floor(r / fontSizeFactor))
+            console.log(context.font)
+            context.fillText(this.data, this.x - fontSize / 4, this.y + fontSize / 4)
+        }
     }
 
     update(cb) {
@@ -103,26 +114,73 @@ class GraphPoint {
     startUpdating(cb) {
         this.state.startUpdating(cb)
     }
+
+    handleTap(x, y) {
+        return this.x - r <= x && x <= this.x + r && y >= this.y - r && y <= this.y + r
+    }
+}
+
+const drawEdgeLine = (context, v1, v2) => {
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.lineCap = 'round'
+    context.strokeStyle = foreColor
+    context.beginPath()
+    context.moveTo(v1.x, v1.y)
+    context.lineTo(v2.x, v2.y)
+    context.stroke()
 }
 
 class GraphRenderer {
     constructor() {
         this.graph = new Graph()
+        this.animator = new Animator()
+        this.edgeStarted = false
+        this.currVertex = null
     }
 
     draw(context) {
+        this.graph.getEdges((edge) => {
+            drawEdgeLine(context, edge.v1.data, edge.v2.data)
+        })
+
         this.graph.getVertices((vertex) => {
-            vertex.draw(context)
+            vertex.data.draw(context)
+        })
+    }
+
+    handleTapInsideGraphVertex(x, y, cb) {
+        this.graph.getVertices((vertex) => {
+            if (vertex.data.handleTap(x, y)) {
+                cb(vertex)
+            }
         })
     }
 
     handleTap(x, y, cb) {
+        if (this.edgeStarted) {
+            this.handleTapInsideGraphVertex(x, y, (vertex) => {
+                console.log(this.currVertex)
+                this.graph.addEdge(this.currVertex.data.data, vertex.data.data)
+                this.currVertex = null
+                this.edgeStarted = false
+                cb()
+            })
+            return
+        }
+        this.handleTapInsideGraphVertex(x, y, (vertex) => {
+            this.currVertex = vertex
+            this.edgeStarted = true
+        })
+
+        if (this.currVertex != null) {
+            return
+        }
         const graphPoint = new GraphPoint(data++, x, y)
-        this.graph.addVertex(graphPoint)
+        this.graph.addVertexNode(graphPoint.data, graphPoint)
         graphPoint.startUpdating(() => {
             this.animator.start(() => {
                 cb()
-                this.graph.update(() => {
+                graphPoint.update(() => {
                     this.animator.stop()
                     cb()
                 })
@@ -130,3 +188,7 @@ class GraphRenderer {
         })
     }
 }
+
+const stage = new Stage()
+stage.render()
+stage.handleTap()
