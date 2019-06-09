6bf849a9e6c96c3cc3bc71cdb267655c9e6903fe
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 15:45:27 +0530

    adding Stage class

---
+++ DrawingUtil.js
@@ -0,0 +1,56 @@
+const w = window.innerWidth
+const h = window.innerHeight
+const scGap = 0.05
+const strokeFactor = 90
+const sizeFactor = 2.9
+const backColor = '#bdbdbd'
+
+class Stage {
+    constructor() {
+        this.canvas = document.createElement('canvas')
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
+
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += this.dir * scGap
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
